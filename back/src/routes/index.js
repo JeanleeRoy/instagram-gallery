c3795f56e9ab84dc2e266064e6f49c4da66e3a37
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2021-04-09 17:21:25 -0500

    [back] add insta-media route

--- back/src/routes/index.js
+++ back/src/routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
+const instaApi = require('../services/instaApi')
 const router = express.Router();
-const https = require('https')
 
 // credentials
-const {clientId, clientSrcret} = require('../services/keys').instagram;
+let token = 'IGQVJWTlJ2MVQtLWl3WFh6dmE5SndhZAUNqTUlKVGdheUpWZAmQ0YVBLbTBCRnRjeU1BSElrZADVGZAGw2aGx3eU5Ddy1kQnRQNkVTTXFUUkkzNGN5QUxDUkRLY0UydlE5czM3SXROQlptMlNwZAi0wTGZAiS1EwTzN5OFRjOVVn'
 
 router.get('/', (req, res) => {
     res.send("index Page");
@@ -12,5 +12,13 @@ router.get('/', (req, res) => {
 router.get('/handleauth', (req, res) => {
     res.json({ user: 'me' });
 });
+
+router.get('/insta-media', (req, res) => {
+    instaApi(token, media => {
+        res.send(media.data.data);
+    }, error => {
+        res.status(404).json({ message: error.message });
+    });
+})
 
 module.exports = router;
