76d201813c1958aa17b2adbffe7f30c8dd37923a
JeanleeRoy <jeanlee.barreto@utec.edu.pe>
2021-04-09 23:45:25 -0500

    [front] add instagram gallery media

--- front/main.js
+++ front/main.js
@@ -2,14 +2,25 @@ const baseURL = 'http://localhost:3000/'
 const instaAPI = baseURL + 'insta-media';
 
 const $gallery = document.getElementById('gallery');
+const $template = document.querySelector('#image-template').content;
+const $fragment = document.createDocumentFragment();
 
 const getInstaMedia = () => {
-    fetch(instaAPI)
+    fetch('./assets/data.json')
     .then(res => res.ok ? res.json() : Promise.reject(res))
-    .then(json => {
-        console.log(json);
+    .then(data => {
+        data.forEach(media => {
+            $template.querySelector(".image").src = media.media_url;
+            $template.querySelector(".image").alt = media.media_type;
+
+            let clone = document.importNode($template, true);
+            $fragment.appendChild(clone);
+        });
+        $gallery.appendChild($fragment);
+
     })
     .catch(err => {
+        console.log(err)
         let message = err.message || err.statusText || "Ocurrió un error";
         let code = err.status || '';
         $gallery.innerHTML = `<p>Error ${code}: ${message}</p>`;
